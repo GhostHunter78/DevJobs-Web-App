cbd82261ccbd3f134789a8f1ba56039b06185332
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-02-28 15:04:19 +0400

    feat: project set up

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <div
+      className="w-screen pt-8 px-6 pb-[72px]"
+      style={{
+        backgroundImage: 'url("/mobile/mobile-header-background.png")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      aeg
+    </div>
+  );
+};
+
+export default Header;
